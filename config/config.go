2811e47357cfe071b669b68e5f7bcaee2f662ed0
Sho Mizutani <lowply@gmail.com>
2015-05-08 22:57:04 +0900

    add permission bit restriction for config file

--- config/config.go
+++ config/config.go
@@ -20,9 +20,15 @@ func GetConfig() Config {
 	var c Config
 	path := "/etc/webhook.hcl"
 
-	_, err := os.Stat(path)
+	// Fail if config does not exist
+	stat, err := os.Stat(path)
 	if err != nil {
 		log.Fatalf("Error reading %s: %s", path, err)
+	}
+
+	// Fail if config file is readable from other users
+	if stat.Mode() != 0600 {
+		log.Fatalf("Permission bits of config file %s should be 0600.", path)
 	}
 
 	d, err := ioutil.ReadFile(path)
